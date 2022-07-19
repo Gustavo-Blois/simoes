00d62f9e23e46cc311892d4e22963539ec0558bf
Gustavo-Blois <gustavobloisrr@gmail.com>
2022-07-19 15:33:15 -0300

    xd

--- jogo.c
+++ jogo.c
@@ -7,7 +7,7 @@ void main(){
 
 
     char mapa[30][30] = {
-        {adksdka´sd ká}
+        {KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK}
         
         
         
